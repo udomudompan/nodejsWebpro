e3341857eb4770f470138f2b7c54872a5057d887
udomudompan <60595810+udomudompan@users.noreply.github.com>
2020-02-24 14:46:36 +0700

    edited

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/product');
-var blogRouter = require('./routes/blog')
+var blogRouter = require('./routes/blog');
 
 var app = express();
 
